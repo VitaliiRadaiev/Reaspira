db6e26c74da25c3bb150665d7e55b0e84cb765eb
Vitalii Radaiev <hf.chelenger@gamil.com>
2020-12-07 11:32:50 +0200

    fix

--- files/js/app.js
+++ files/js/app.js
@@ -92,7 +92,7 @@ $('img.img-svg').each(function(){
 
 
 //Spollers
-
+function spollerStart() {
 	let spollers = document.querySelectorAll("._spoller");
 	if (spollers.length > 0) {
 		for (let index = 0; index < spollers.length; index++) {
@@ -120,7 +120,8 @@ $('img.img-svg').each(function(){
 			});
 		}
 	}
-
+}
+spollerStart();
 // === // Spollers ==================================================================
 
 
@@ -292,6 +293,167 @@ document.addEventListener('keydown', function(e) {
 		}
 	})();
 // === AND Polyfill ===;
+	
+
+//Select
+let selects = document.getElementsByTagName('select');
+if (selects.length > 0) {
+	selects_init();
+}
+function selects_init() {
+	for (let index = 0; index < selects.length; index++) {
+		const select = selects[index];
+		select_init(select);
+	}
+	//select_callback();
+	document.addEventListener('click', function (e) {
+		selects_close(e);
+	});
+	document.addEventListener('keydown', function (e) {
+		if (e.which == 27) {
+			selects_close(e);
+		}
+	});
+}
+function selects_close(e) {
+	const selects = document.querySelectorAll('.select');
+	if (!e.target.closest('.select')) {
+		for (let index = 0; index < selects.length; index++) {
+			const select = selects[index];
+			const select_body_options = select.querySelector('.select__options');
+			select.classList.remove('_active');
+			_slideUp(select_body_options, 100);
+		}
+	}
+}
+function select_init(select) {
+	const select_parent = select.parentElement;
+	const select_modifikator = select.getAttribute('class');
+	const select_selected_option = select.querySelector('option:checked');
+	select.setAttribute('data-default', select_selected_option.value);
+	select.style.display = 'none';
+
+	select_parent.insertAdjacentHTML('beforeend', '<div class="select select_' + select_modifikator + '"></div>');
+
+	let new_select = select.parentElement.querySelector('.select');
+	new_select.appendChild(select);
+	select_item(select);
+}
+function select_item(select) {
+	const select_parent = select.parentElement;
+	const select_items = select_parent.querySelector('.select__item');
+	const select_options = select.querySelectorAll('option');
+	const select_selected_option = select.querySelector('option:checked');
+	const select_selected_text = select_selected_option.text;
+	const select_type = select.getAttribute('data-type');
+
+	if (select_items) {
+		select_items.remove();
+	}
+
+	let select_type_content = '';
+	if (select_type == 'input') {
+		select_type_content = '<div class="select__value icon-select-arrow"><input autocomplete="off" type="text" name="form[]" value="' + select_selected_text + '" data-error="Ошибка" data-value="' + select_selected_text + '" class="select__input"></div>';
+	} else {
+		select_type_content = '<div class="select__value icon-select-arrow"><span>' + select_selected_text + '</span></div>';
+	}
+
+	select_parent.insertAdjacentHTML('beforeend',
+		'<div class="select__item">' +
+		'<div class="select__title">' + select_type_content + '</div>' +
+		'<div class="select__options">' + select_get_options(select_options) + '</div>' +
+		'</div></div>');
+
+	select_actions(select, select_parent);
+}
+function select_actions(original, select) {
+	const select_item = select.querySelector('.select__item');
+	const select_body_options = select.querySelector('.select__options');
+	const select_options = select.querySelectorAll('.select__option');
+	const select_type = original.getAttribute('data-type');
+	const select_input = select.querySelector('.select__input');
+
+	select_item.addEventListener('click', function () {
+		let selects = document.querySelectorAll('.select');
+		for (let index = 0; index < selects.length; index++) {
+			const select = selects[index];
+			const select_body_options = select.querySelector('.select__options');
+			if (select != select_item.closest('.select')) {
+				select.classList.remove('_active');
+				_slideUp(select_body_options, 100);
+			}
+		}
+		_slideToggle(select_body_options, 100);
+		select.classList.toggle('_active');
+	});
+
+	for (let index = 0; index < select_options.length; index++) {
+		const select_option = select_options[index];
+		const select_option_value = select_option.getAttribute('data-value');
+		const select_option_text = select_option.innerHTML;
+
+		if (select_type == 'input') {
+			select_input.addEventListener('keyup', select_search);
+		} else {
+			if (select_option.getAttribute('data-value') == original.value) {
+				select_option.style.display = 'none';
+			}
+		}
+		select_option.addEventListener('click', function () {
+			for (let index = 0; index < select_options.length; index++) {
+				const el = select_options[index];
+				el.style.display = 'block';
+			}
+			if (select_type == 'input') {
+				select_input.value = select_option_text;
+				original.value = select_option_value;
+			} else {
+				select.querySelector('.select__value').innerHTML = '<span>' + select_option_text + '</span>';
+				original.value = select_option_value;
+				select_option.style.display = 'none';
+			}
+		});
+	}
+}
+function select_get_options(select_options) {
+	if (select_options) {
+		let select_options_content = '';
+		for (let index = 0; index < select_options.length; index++) {
+			const select_option = select_options[index];
+			const select_option_value = select_option.value;
+			if (select_option_value != '') {
+				const select_option_text = select_option.text;
+				select_options_content = select_options_content + '<div data-value="' + select_option_value + '" class="select__option">' + select_option_text + '</div>';
+			}
+		}
+		return select_options_content;
+	}
+}
+function select_search(e) {
+	let select_block = e.target.closest('.select ').querySelector('.select__options');
+	let select_options = e.target.closest('.select ').querySelectorAll('.select__option');
+	let select_search_text = e.target.value.toUpperCase();
+
+	for (let i = 0; i < select_options.length; i++) {
+		let select_option = select_options[i];
+		let select_txt_value = select_option.textContent || select_option.innerText;
+		if (select_txt_value.toUpperCase().indexOf(select_search_text) > -1) {
+			select_option.style.display = "";
+		} else {
+			select_option.style.display = "none";
+		}
+	}
+}
+function selects_update_all() {
+	let selects = document.querySelectorAll('select');
+	if (selects) {
+		for (let index = 0; index < selects.length; index++) {
+			const select = selects[index];
+			select_item(select);
+		}
+	}
+}
+;
 
 
 // === Проверка, поддержка браузером формата webp ==================================================================
@@ -390,7 +552,7 @@ cardVideoHandler();;
 			speed: 800,
 			//touchRatio: 0,
 			//simulateTouch: false,
-			loop: true,
+			//loop: true,
 			//preloadImages: false,
 			//lazy: true,
 			// Dotts
@@ -406,17 +568,26 @@ cardVideoHandler();;
 			
 			breakpoints: {
 				320: {
-					slidesPerView: 1,
+					slidesPerView: 2,
+					spaceBetween: 5,
+					slidesPerColumn: 2,
+					autoHeight: false,
+				},
+				480: {
+					slidesPerView: 2,
 					spaceBetween: 15,
-					autoHeight: true,
+					slidesPerColumn: 2,
+					autoHeight: false,
 				},
 				768: {
 					slidesPerView: 2,
 					spaceBetween: 20,
+					slidesPerColumn: 1,
 				},
 				992: {
 					slidesPerView: 3,
 					spaceBetween: 25,
+					slidesPerColumn: 1,
 				},
 
 			},
@@ -448,5 +619,47 @@ cardVideoHandler();;
   }
 }
 ;
+function recipesAddSpollers() {
+	let list = document.querySelector('.recipes__list');
+
+	if(list.classList.contains('_spollers')) {
+		return
+	} else {
+		let labels = list.querySelectorAll('.recipes__label');
+
+		list.classList.add('_spollers');
+		labels.forEach(item => {
+			item.classList.add('_spoller');
+		})
+		spollerStart();
+	}
+}
+
+function recipesRemoveSpollers() {
+	let list = document.querySelector('.recipes__list');
+	let labels = list.querySelectorAll('.recipes__label');
+
+	list.classList.remove('_spollers');
+	labels.forEach(item => {
+		item.classList.remove('_spoller');
+	})
+}
+
+{
+	let recipes = document.querySelector('.recipes');
+	if(recipes) {
+		if(document.documentElement.clientWidth < 768) {
+			recipesAddSpollers();
+					} else {
+			window.addEventListener('resize', () => {
+				if(document.documentElement.clientWidth < 768) {
+					recipesAddSpollers();
+				} else {
+					recipesRemoveSpollers();
+				}
+			})
+		}
+	}
+};
 
 });
